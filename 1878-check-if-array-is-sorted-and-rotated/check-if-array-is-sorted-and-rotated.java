c54e2e50a2fdd6e7e622d7bcf41812b6fec7f243
Ronit Singh <167860954+RonitSingh01@users.noreply.github.com>
2025-10-11 20:26:51 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (29.43%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,21 +1,13 @@
 class Solution {
-    public boolean check(int[] arr) {
-        int[] copyArr = Arrays.copyOf(arr,arr.length);
-        int n = arr.length;
-        Arrays.sort(copyArr);
-        ArrayList<Integer> al = new ArrayList<>();
-        for(int i: arr){
-            al.add(i);
+    public boolean check(int[] nums) {
+        int count=0;
+        int n = nums.length;
+        for(int i=0;i<n;i++){
+            if(nums[i]>nums[(i+1)%n]) count++;
+            if(count>1) return false;
         }
-        ArrayList<Integer> al1 = new ArrayList<>();
-        for(int i: copyArr){
-            al1.add(i);
-        }
-        for(int r=0;r<n;r++){
-            Collections.rotate(al,1);
-            if(al.equals(al1)) return true;
-
-        }
-        return false;
+        return true;
+    
+        
     }
 }
